5a10bcad7e9a5073c05ceb56d1feb7a786926ce1
anass <anass.elkadiri98@gmail.com>
2023-07-18 14:50:22 -0700

    zabi

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ made by ProdbyElPadre
+ */
+int print_last_digit(int n)
+{
+	int a;
+
+	if (n < 0)
+	n = -n;
+
+	a = n % 10;
+
+	if (a < 0)
+		a = -a;
+
+	_putchar(a + '0');
+
+	return (a);
+}
+
